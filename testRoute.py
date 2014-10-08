d929286972bd6e056b1f958bc9d9da939f050c84
Javier Quinteros <javiquinte@gmail.com>
2014-10-08 15:53:45 +0200

    Add more test cases from the examples in the documentation.

--- testRoute.py
+++ testRoute.py
@@ -4,6 +4,7 @@ import sys
 import unittest
 from unittestTools import WITestRunner
 from routing import RoutingCache
+from routing import RequestMerge
 
 
 class RouteCacheTests(unittest.TestCase):
@@ -22,24 +23,111 @@ class RouteCacheTests(unittest.TestCase):
     def testDS_GE(self):
         "Dataselect GE.*.*.*"
 
-        expectedURL = 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query'
+        expURL = 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query'
         result = self.rc.getRoute('GE')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
         self.assertEqual(len(result), 1,
                          'Wrong number of data centers for GE.*.*.*!')
-        self.assertEqual(result[0]['url'], expectedURL,
-                         'Wrong URL for GE network!')
+        self.assertEqual(result[0]['url'], expURL,
+                         'Wrong URL for GE.*.*.*')
         self.assertEqual(result[0]['name'], 'dataselect',
                          'Wrong service name!')
 
     def testDS_GE_APE(self):
         "Dataselect GE.APE.*.*"
 
-        expectedURL = 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query'
+        expURL = 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query'
         result = self.rc.getRoute('GE', 'APE')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
         self.assertEqual(len(result), 1,
                          'Wrong number of data centers for GE.APE.*.*!')
-        self.assertEqual(result[0]['url'], expectedURL,
-                         'Wrong URL for GE network!')
+        self.assertEqual(result[0]['url'], expURL,
+                         'Wrong URL for GE.APE.*.*!')
+        self.assertEqual(result[0]['name'], 'dataselect',
+                         'Wrong service name!')
+
+    def testDS_CH_LIENZ_HHZ(self):
+        "Dataselect CH.LIENZ.*.HHZ"
+
+        expURL = 'http://eida.ethz.ch/fdsnws/dataselect/1/query'
+        result = self.rc.getRoute('CH', 'LIENZ', '*', 'HHZ')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
+        self.assertEqual(len(result), 1,
+                         'Wrong number of data centers for CH.LIENZ.*.HHZ!')
+        self.assertEqual(result[0]['url'], expURL,
+                         'Wrong URL for CH.LIENZ.*.HHZ!')
+        self.assertEqual(result[0]['name'], 'dataselect',
+                         'Wrong service name!')
+
+    def testDS_CH_LIENZ_BHZ(self):
+        "Dataselect CH.LIENZ.*.BHZ"
+
+        expURL = 'http://www.orfeus-eu.org/fdsnws/dataselect/1/query'
+        result = self.rc.getRoute('CH', 'LIENZ', '*', 'BHZ')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
+        self.assertEqual(len(result), 1,
+                         'Wrong number of data centers for CH.LIENZ.*.BHZ!')
+        self.assertEqual(result[0]['url'], expURL,
+                         'Wrong URL for CH.LIENZ.*.BHZ!')
+        self.assertEqual(result[0]['name'], 'dataselect',
+                         'Wrong service name!')
+
+    def testDS_CH_LIENZ_qHZ(self):
+        "Dataselect CH.LIENZ.*.?HZ"
+
+        odcURL = 'http://www.orfeus-eu.org/fdsnws/dataselect/1/query'
+        ethURL = 'http://eida.ethz.ch/fdsnws/dataselect/1/query'
+        result = self.rc.getRoute('CH', 'LIENZ', '*', '?HZ')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
+        self.assertEqual(len(result), 2,
+                         'Wrong number of data centers for CH.LIENZ.*.?HZ!')
+
+        for res in result:
+            if 'eth' in res['url']:
+                self.assertEqual(res['url'], ethURL,
+                                 'Wrong URL for CH.LIENZ.*.?HZ!')
+                self.assertEqual(res['name'], 'dataselect',
+                                 'Wrong service name!')
+
+                myStreams = ['LHZ', 'HHZ']
+                self.assertEqual(len(res['params']), len(myStreams),
+                                 'Wrong number of streams for ETH!')
+
+                for i in res['params']:
+                    self.assertIn(i['cha'], myStreams,
+                                  '%s is not an expected channel for ETH!'
+                                  % i['cha'])
+            elif 'orfeus' in res['url']:
+                self.assertEqual(res['url'], odcURL,
+                                 'Wrong URL for CH.LIENZ.*.?HZ!')
+                self.assertEqual(res['name'], 'dataselect',
+                                 'Wrong service name!')
+
+                self.assertEqual(len(res['params']), 1,
+                                 'Wrong number of streams for ODC!')
+                self.assertIn(res['params'][0]['cha'], 'BHZ',
+                              '%s is not an expected channel for ETH!' %
+                              res['params'][0]['cha'])
+            else:
+                self.assertEqual(1, 0,
+                                 'None of the URLs belong to Orfeus or ETH!')
+
+    def testDS_RO_BZS_BHZ(self):
+        "Dataselect RO.BZS.*.BHZ"
+
+        expURL = 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query'
+        result = self.rc.getRoute('RO', 'BZS', '*', 'BHZ')
+        self.assertIsInstance(result, RequestMerge,
+                              'A RequestMerge object was expected!')
+        self.assertEqual(len(result), 1,
+                         'Wrong number of data centers for RO.BZS.*.BHZ!')
+        self.assertEqual(result[0]['url'], expURL,
+                         'Wrong URL for RO.BZS.*.BHZ!')
         self.assertEqual(result[0]['name'], 'dataselect',
                          'Wrong service name!')
 
