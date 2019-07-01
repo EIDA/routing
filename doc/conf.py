6f23f8f5dbc336623a8bacb91221e9874aedfbdc
Javier Quinteros <javier@quinteros.eu>
2019-07-01 18:46:32 +0200

    Improve the output and options for ReadTheDocs.

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Routing-WS'
-copyright = u'2014-2017, Javier Quinteros'
+copyright = u'2014-2019, Javier Quinteros - Deutsches GFZ Potsdam'
+author = 'Javier Quinteros'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -107,14 +108,18 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
-
+html_theme_options = {
+    'collapsiblesidebar': True,
+    'externalrefs': True,
+    'navigation_depth': 2,
+    'issues_url': 'https://github.com/EIDA/routing/issues'
+}
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = 'Routing Service Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
