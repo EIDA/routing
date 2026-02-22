7dd02b196f6401ff3702b4a2e6521ce83dd299ee
Javier Quinteros <javier@quinteros.eu>
2026-02-22 23:15:47 +0100

    Add first test for query method

--- routing/test_routing.py
+++ routing/test_routing.py
@@ -60,3 +60,58 @@ def test_dc():
     assert "fullName" in response.json()
     assert "summary" in response.json()
     assert "repositories" in response.json()
+
+def test_dataselect_get_GE_noformat():
+    response = client.get("/query", params={"net": "GE"})
+    assert response.status_code == 200
+    assert '<url>https://geofon.gfz.de/fdsnws/dataselect/1/query</url>' in response.text
+    assert '<net>GE</net>' in response.text
+    assert '<priority>1</priority>' in response.text
+
+def test_dataselect_get_GE_xml():
+    response = client.get("/query", params={"net": "GE", "format": "xml"})
+    assert response.status_code == 200
+    assert '<url>https://geofon.gfz.de/fdsnws/dataselect/1/query</url>' in response.text
+    assert '<net>GE</net>' in response.text
+    assert '<priority>1</priority>' in response.text
+
+def test_dataselect_get_GE_post():
+    response = client.get("/query", params={"net": "GE", "format": "post"})
+    assert response.status_code == 200
+    assert 'https://geofon.gfz.de/fdsnws/dataselect/1/query' in response.text
+    assert 'GE * * * 1993-01-01' in response.text
+
+def test_dataselect_get_GE_get():
+    response = client.get("/query", params={"net": "GE", "format": "get"})
+    assert response.status_code == 200
+    assert response.text == 'https://geofon.gfz.de/fdsnws/dataselect/1/query?net=GE&start=1993-01-01T00:00:00'
+
+def test_station_get_GE_noformat():
+    response = client.get("/query", params={"net": "GE", "service": "station"})
+    assert response.status_code == 200
+    assert '<url>https://geofon.gfz.de/fdsnws/station/1/query</url>' in response.text
+    assert '<net>GE</net>' in response.text
+    assert '<priority>1</priority>' in response.text
+
+def test_station_get_GE_xml():
+    response = client.get("/query", params={"net": "GE", "service": "station", "format": "xml"})
+    assert response.status_code == 200
+    assert '<url>https://geofon.gfz.de/fdsnws/station/1/query</url>' in response.text
+    assert '<net>GE</net>' in response.text
+    assert '<priority>1</priority>' in response.text
+
+def test_station_get_GE_post():
+    response = client.get("/query", params={"net": "GE", "service": "station", "format": "post"})
+    assert response.status_code == 200
+    assert 'https://geofon.gfz.de/fdsnws/station/1/query' in response.text
+    assert 'GE * * * 1993-01-01' in response.text
+
+def test_station_get_GE_get():
+    response = client.get("/query", params={"net": "GE", "service": "station", "format": "get"})
+    assert response.status_code == 200
+    assert response.text == 'https://geofon.gfz.de/fdsnws/station/1/query?net=GE&start=1993-01-01T00:00:00'
+
+def test_dataselect_get_4C_KES27_HNZ_get():
+    response = client.get("/query", params={"net": "4C", "sta": "KES27", "cha": "HNZ", "format": "get"})
+    assert response.status_code == 200
+    assert response.text == 'https://geofon.gfz.de/fdsnws/dataselect/1/query?net=4C&sta=KES27&cha=HNZ&start=2011-09-15T00:00:00&end=2012-04-20T23:59:00'
